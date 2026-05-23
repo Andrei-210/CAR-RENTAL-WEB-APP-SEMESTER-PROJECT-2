4b0b712b8898d6cf2c078a5e16b33e3f6ffeaeb6
cristian florin cojocaru <cristiancojocaru04@yahoo.com>
2026-05-24 01:42:23 +0300

    Revert "Merge branch 'Andrei-210:main' into main"
    This reverts commit 85644a663b1658eb318e9a0606a94a22afd83d32, reversing
    changes made to 91430e656a37ca13bc0e7907b5ee19b7b0bcf5ba.

--- car-rental-web-app.client/wheeldeal.spec.ts
+++ car-rental-web-app.client/wheeldeal.spec.ts
@@ -475,8 +475,3 @@ test('A06 - GET /reports/dashboard returns stats with valid admin token', async 
   expect(body).toHaveProperty('todayRevenue');
   expect(body.totalVehicles).toBeGreaterThan(0);
 });
-
-test.afterAll(async () => {
-  // mic buffer pentru cleanup
-  await new Promise(res => setTimeout(res, 100));
-});
